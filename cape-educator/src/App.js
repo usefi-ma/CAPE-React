8934fa6ecedb6bc92c9df2a55ca993f462f863ae
Avininder <avirao22g@gmail.com>
2023-03-13 22:08:57 -0600

    changing research to react
    to react

--- cape-educator/src/App.js
+++ cape-educator/src/App.js
@@ -1,12 +1,14 @@
 // import './App.css';
 import Home from './user-pages/Home';
 import About from './user-pages/About';
+import Research from './user-pages/Research';
 
 function App() {
   return (
     <>
       {/* <Home></Home> */}
       <About></About>
+      <Research></Research>
     </>
   );
 }
