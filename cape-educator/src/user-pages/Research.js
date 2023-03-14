468f3ef0117bac8851ab3f6e57b9d6bb4589e7a1
Avininder <avirao22g@gmail.com>
2023-03-13 21:50:04 -0600

    react
    changing react

---
+++ cape-educator/src/user-pages/Research.js
@@ -0,0 +1,130 @@
+import React from 'react';
+
+import "../assest/css/index.css";
+import "../assest/css/pages/research.css";
+
+import UserHeader from '../layouts/UserHeader';
+import UserFooter from '../layouts/UserFooter';
+
+function Research(){
+    return(
+        <>
+        <UserHeader></UserHeader>
+        
+        <div>
+    <section className="objective">
+        <div className="container">
+            <div className="row">
+                <h2 className="section_title">
+                    Research
+                </h2>
+            </div>
+            <div className="row">
+                <div className="col-12 col-sm-12 col-md-6 col-lg-4 gx-lg-5 mb-5">
+                    <div className="obj_box">
+                        <div className="box_title">
+                            <div className="icon_wrapp">
+                                <img src="/images/network/justiceinstitute.png" />
+                            </div>
+                            <h3>Exposures to Potentially Traumatic Events Among Public Safety Personnel in Canada</h3>
+                        </div>
+                        <div className="box_desc">
+                            <p>
+                                December 2018Canadian Journal of Behavioural Science
+                                <br />
+                                DOI: 10.1037/cbs0000115.
+                            </p>
+                            <a href="https://www.researchgate.net/publication/329550261_Exposures_to_Potentially_Traumatic_Events_Among_Public_Safety_Personnel_in_Canada" className="text_gradient" target="_blank">
+                                Read More
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-sm-12 col-md-6 col-lg-4 gx-lg-5 mb-5">
+                    <div className="obj_box">
+                        <div className="box_title">
+                            <div className="icon_wrapp">
+                                <img src="/images/network/justiceinstitute.png" />
+                            </div>
+                            <h3>Mental health training, attitudes toward support, and screening positive for mental disorders</h3>
+                        </div>
+                        <div className="box_desc">
+                            <p>
+                                February 2019 Cognitive Behaviour Therapy
+                                <br />
+                                DOI: 10.1080/16506073.2019.1575900
+                            </p>
+                            <a href="https://www.researchgate.net/publication/331292873_Mental_health_training_attitudes_toward_support_and_screening_positive_for_mental_disorders" className="text_gradient" target="_blank">
+                                Read More
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-sm-12 col-md-6 col-lg-4 gx-lg-5 mb-5">
+                    <div className="obj_box">
+                        <div className="box_title">
+                            <div className="icon_wrapp">
+                                <img src="/images/network/CAN-SEBP.png" />
+                            </div>
+                            <h3> The Canadian Society of Evidence-Based Policing</h3>
+                        </div>
+                        <div className="box_desc">
+                            <p>
+                            </p>
+                            <a href="https://www.can-sebp.net/" className="text_gradient" target="_blank">
+                                Read More
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-sm-12 col-md-6 col-lg-4 gx-lg-5 mb-5">
+                    <div className="obj_box">
+                        <div className="box_title">
+                            <div className="icon_wrapp">
+                                <img src="/images/network/cpkn-rcsp.png" />
+                            </div>
+                            <h3>Canadian Police Knowledge Network</h3>
+                        </div>
+                        <div className="box_desc">
+                            <p>
+                            </p>
+                            <a href="https://www.cpkn.ca/en/" className="text_gradient" target="_blank">
+                                Read More
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+</div>
+        
+        <UserFooter></UserFooter>
+        </>
+    );
+    
+}
+
+
+export default Research;
