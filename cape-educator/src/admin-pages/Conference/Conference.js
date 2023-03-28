a9641aa005975da608294594d89856948931b621
usefi-ma <usefi.ma@gmail.com>
2023-03-28 11:37:22 -0600

    fixing scroll

--- cape-educator/src/admin-pages/Conference/Conference.js
+++ cape-educator/src/admin-pages/Conference/Conference.js
@@ -1,8 +1,124 @@
 import React from 'react'
+import { Grid, Container, Typography, Card, Button } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import CardContent from "@mui/material/CardContent";
+import Box from "@mui/material/Box";
 
 const Conference = () => {
   return (
-    <>Conference</>
+    <>
+     <Container maxWidth="xl">
+        <Typography variant="h4" sx={{ marginBottom: 3 }}>
+          Executive
+        </Typography>
+        <Box component="form" noValidate autoComplete="off">
+          <Card sx={{ padding: 3 }}>
+            <CardContent>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6} md={6}>
+                  <Typography
+                    variant="subtitle1"
+                    style={{
+                      marginBottom: 8,
+                      display: "block",
+                      fontWeight: "500",
+                    }}
+                  >
+                    Full Name
+                  </Typography>
+                  <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Full Name"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6} md={6}>
+                  <Typography
+                    variant="subtitle1"
+                    style={{
+                      marginBottom: 8,
+                      display: "block",
+                      fontWeight: "500",
+                    }}
+                  >
+                    Job Title
+                  </Typography>
+                  <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Job Title"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography
+                    variant="subtitle1"
+                    style={{
+                      marginBottom: 8,
+                      display: "block",
+                      fontWeight: "500",
+                    }}
+                  >
+                    Organization
+                  </Typography>
+                  <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Organization"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography
+                    variant="subtitle1"
+                    style={{
+                      marginBottom: 8,
+                      display: "block",
+                      fontWeight: "500",
+                    }}
+                  >
+                    Image
+                  </Typography>
+                  <Button variant="contained" component="label" organization="large">
+                    Upload
+                    <input hidden accept="image/*" multiple type="file" />
+                  </Button>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography
+                    variant="subtitle1"
+                    style={{
+                      marginBottom: 8,
+                      display: "block",
+                      fontWeight: "500",
+                    }}
+                  >
+                    Description
+                  </Typography>
+                  <TextField
+                    id="outlined-multiline-static"
+                    label="Description"
+                    multiline
+                    rows={5}
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Button variant="contained" organization="large">
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Box>
+        
+      </Container>
+    
+    </>
   )
 }
 
