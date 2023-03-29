c1e3f259456c2cf3d57b679422132c1e6cb36070
Rajnandani-Manocha <116530470+Rajnandani-Manocha@users.noreply.github.com>
2023-03-29 14:29:03 -0600

    Banners Page

--- cape-educator/src/admin-pages/Banner.js
+++ cape-educator/src/admin-pages/Banner.js
@@ -1,57 +1,173 @@
-import {
-  Card,
-  Stack,
-  Container,
-  Typography
-} from '@mui/material';
-import { styled } from '@mui/material/styles';
+import React from "react";
+import { Grid, Container, Typography, Card, Button } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import CardContent from "@mui/material/CardContent";
+import Box from "@mui/material/Box";
 
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+const columns = [
+  { id: 'banner', label: 'Banner Title', minWidth: 170 },
+  { id: 'eventTitle', label: 'Event Title', minWidth: 100 },
+  { id: 'date', label: 'Date', minWidth: 170 },
+  { id: 'link', label: 'Link URL', minWidth: 100 },
+  {
+    id: 'Description',
+    label: 'Event Title Description',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toFixed(2),
+  },
+  { id: 'image', label: 'Banner Image', minWidth: 170 },
+  {
+    id: 'action',
+    label: 'Action',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toFixed(2),
+  },
+];
 
+function createData(banner, eventTitle, date, link, image, action) {
+  const Description =  eventTitle;
+  return { banner, eventTitle, date, link, Description, image, action };
+}
 
-
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
 
 export default function Banner() {
 
   return (
     <>
-      <Container>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-          <Typography variant="h4" gutterBottom>
-            User
-          </Typography>
-        </Stack>
-        <Card>
-        <Box sx={{ flexGrow: 1 }}  gap={2}>
-      <Grid container spacing={2}>
-        <Grid item xs={6}  p={2}>
-          <Item>xs=6</Item>
-        </Grid>
-        <Grid item xs={6}  p={2}>
-          <Item>xs=6</Item>
-        </Grid>
-        <Grid item xs={6}  p={2}>
-          <Item>xs=6</Item>
-        </Grid>
-        <Grid item xs={6}  p={2}>
-          <Item>xs=6</Item>
-        </Grid>
-      </Grid>
-    </Box>
-        
-        </Card>
-      </Container>
-     
+
+<Container maxWidth="xl">
+        <Typography variant="h4" sx={{ marginBottom: 3 }}>
+          User
+        </Typography>
+        <Box component="form" noValidate autoComplete="off">
+          <Card sx={{ padding: 3 }}>
+            <CardContent>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6} md={6}>
+                  <Typography
+                    variant="subtitle1"
+                    style={{
+                      marginBottom: 8,
+                      display: "block",
+                      fontWeight: "500",
+                    }}
+                  >
+                    Banner Title
+                  </Typography>
+                  <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Banner Title"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6} md={6}>
+                  <Typography
+                    variant="subtitle1"
+                    style={{
+                      marginBottom: 8,
+                      display: "block",
+                      fontWeight: "500",
+                    }}
+                  >
+                    Event Title
+                  </Typography>
+                  <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Event Title"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography
+                    variant="subtitle1"
+                    style={{
+                      marginBottom: 8,
+                      display: "block",
+                      fontWeight: "500",
+                    }}
+                  >
+                    Date
+                  </Typography>
+                  <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Date"
+                    variant="outlined"
+                    type="date"
+                    InputLabelProps={{
+                    shrink: true,
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6} md={6}>
+                  <Typography
+                    variant="subtitle1"
+                    style={{
+                      marginBottom: 8,
+                      display: "block",
+                      fontWeight: "500",
+                    }}
+                  >
+                    Link URL
+                  </Typography>
+                  <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Link URL"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography
+                    variant="subtitle1"
+                    style={{
+                      marginBottom: 8,
+                      display: "block",
+                      fontWeight: "500",
+                    }}
+                  >
+                    Banner Image
+                  </Typography>
+                  <Button variant="contained" component="label" size="large">
+                    Upload
+                    <input hidden accept="image/*" multiple type="file" />
+                  </Button>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography
+                    variant="subtitle1"
+                    style={{
+                      marginBottom: 8,
+                      display: "block",
+                      fontWeight: "500",
+                    }}
+                  >
+                    Event Title Description
+                  </Typography>
+                  <TextField
+                    id="outlined-multiline-static"
+                    label="Description"
+                    multiline
+                    rows={3}
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Button variant="contained" size="large">
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Box>
+        </Container>
     </>
   );
 }
