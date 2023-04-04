aedbc5d71939acd2705491e4d89f406bb5709cf6
mendom <51889653+mendom@users.noreply.github.com>
2023-04-03 23:37:11 -0600

    Executive tab logic completed

--- cape-educator/src/admin-pages/Executive.js
+++ cape-educator/src/admin-pages/Executive.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Container, Typography, Card, Button } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import CardContent from "@mui/material/CardContent";
@@ -23,8 +23,8 @@ const columns = [
     format: (value) => value.toLocaleString('en-US'),
   },
   {
-    id: 'Description',
-    label: 'Description',
+    id: 'description',
+    label: 'description',
     minWidth: 170,
     align: 'right',
     format: (value) => value.toFixed(2),
@@ -39,26 +39,26 @@ const columns = [
 ];
 
 function createData(name, jobTitle, organization,action) {
-  const Description =  organization;
-  return { name, jobTitle, organization, Description,action };
+  const description =  organization;
+  return { name, jobTitle, organization, description,action };
 }
 
 const rows = [
-  createData('India', 'IN', 1324171354, 3287263,1),
-  createData('China', 'CN', 1403500365, 9596961,1),
-  createData('Italy', 'IT', 60483973, 301340,1),
-  createData('United States', 'US', 327167434, 9833520,1),
-  createData('Canada', 'CA', 37602103, 9984670,1),
-  createData('Australia', 'AU', 25475400, 7692024,1),
-  createData('Germany', 'DE', 83019200, 357578,1),
-  createData('Ireland', 'IE', 4857000, 70273,1),
-  createData('Mexico', 'MX', 126577691, 1972550,1),
-  createData('Japan', 'JP', 126317000, 377973,1),
-  createData('France', 'FR', 67022000, 640679,1),
-  createData('United Kingdom', 'GB', 67545757, 242495,1),
-  createData('Russia', 'RU', 146793744, 17098246,1),
-  createData('Nigeria', 'NG', 200962417, 923768,1),
-  createData('Brazil', 'BR', 210147125, 8515767,1),
+  // createData('India', 'IN', 1324171354, 3287263,1),
+  // createData('China', 'CN', 1403500365, 9596961,1),
+  // createData('Italy', 'IT', 60483973, 301340,1),
+  // createData('United States', 'US', 327167434, 9833520,1),
+  // createData('Canada', 'CA', 37602103, 9984670,1),
+  // createData('Australia', 'AU', 25475400, 7692024,1),
+  // createData('Germany', 'DE', 83019200, 357578,1),
+  // createData('Ireland', 'IE', 4857000, 70273,1),
+  // createData('Mexico', 'MX', 126577691, 1972550,1),
+  // createData('Japan', 'JP', 126317000, 377973,1),
+  // createData('France', 'FR', 67022000, 640679,1),
+  // createData('United Kingdom', 'GB', 67545757, 242495,1),
+  // createData('Russia', 'RU', 146793744, 17098246,1),
+  // createData('Nigeria', 'NG', 200962417, 923768,1),
+  // createData('Brazil', 'BR', 210147125, 8515767,1),
 ];
 const Executive = () => {
   const [page, setPage] = React.useState(0);
@@ -72,13 +72,58 @@ const Executive = () => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
+
+  //Form Data handle
+  const [executiveData, setExecutiveData] = useState([]);
+  const[formErrors, setFormErrors] = useState({});
+
+  const handleFormSubmit = (e) =>{
+    e.preventDefault();
+
+    const name = e.target.fullName.value.trim();
+    const jobTitle = e.target.jobTitle.value.trim();
+    const organization = e.target.organization.value.trim();
+    const description = e.target.description.value.trim();
+
+    const errors = {};
+    if(!name){
+      errors.name = 'Name is required';
+    }
+    if(!jobTitle){
+      errors.jobTitle = 'Job title is required';
+    }
+    if(!organization){
+      errors.organization = 'Organization is required';
+    }
+    if(!description){
+      errors.description = 'Description is required';
+    }
+
+    if(Object.keys(errors).length > 0){
+      setFormErrors(errors);
+    } else {
+      setExecutiveData([...executiveData, {name, jobTitle, organization, description}]);
+      console.log(executiveData);
+      e.target.reset();
+      setFormErrors({});
+    }
+  }
+
+  //TESTING
+  const handleChange = (e) =>{
+    const { name, value} = e.target;
+    console.log(name, value);
+  }
+
+
+
   return (
     <>
       <Container maxWidth="xl">
         <Typography variant="h4" sx={{ marginBottom: 3 }}>
           Executive
         </Typography>
-        <Box component="form" noValidate autoComplete="off">
+        <Box onSubmit={handleFormSubmit} component="form" noValidate autoComplete="off">
           <Card sx={{ padding: 3 }}>
             <CardContent>
               <Grid container spacing={2}>
@@ -98,6 +143,10 @@ const Executive = () => {
                     id="outlined-basic"
                     label="Full Name"
                     variant="outlined"
+                    name="fullName"
+                    onChange={handleChange}
+                    error={formErrors.name}
+                    helperText={formErrors.name}
                   />
                 </Grid>
                 <Grid item xs={12} sm={6} md={6}>
@@ -116,6 +165,10 @@ const Executive = () => {
                     id="outlined-basic"
                     label="Job Title"
                     variant="outlined"
+                    name="jobTitle"
+                    onChange={handleChange}
+                    error={formErrors.jobTitle}
+                    helperText={formErrors.jobTitle}
                   />
                 </Grid>
                 <Grid item xs={6}>
@@ -134,6 +187,10 @@ const Executive = () => {
                     id="outlined-basic"
                     label="Organization"
                     variant="outlined"
+                    name="organization"
+                    onChange={handleChange}
+                    error={formErrors.organization}
+                    helperText={formErrors.organization}
                   />
                 </Grid>
                 <Grid item xs={6}>
@@ -170,11 +227,15 @@ const Executive = () => {
                     rows={5}
                     variant="outlined"
                     fullWidth
+                    name="description"
+                    onChange={handleChange}
+                    error={formErrors.description}
+                    helperText={formErrors.description}
                   />
                 </Grid>
 
                 <Grid item xs={12}>
-                  <Button variant="contained" size="large">
+                  <Button type="submit" variant="contained" size="large">
                     Submit
                   </Button>
                 </Grid>
@@ -208,13 +269,13 @@ const Executive = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows
+            {executiveData
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
+              .map((data) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.jobTitle}>
+                  <TableRow hover role="checkbox" tabIndex={-1} key={data.jobTitle}>
                     {columns.map((column) => {
-                      const value = row[column.id];
+                      const value = data[column.id];
                       return (
                         <TableCell key={column.id} align={column.align}>
                           {column.format && typeof value === 'number'
